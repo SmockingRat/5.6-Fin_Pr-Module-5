4843422a2884d3d5412699cccccfcaec0ba0625d
SmokingRat <halfbrikbarry@gmail.com>
2022-03-10 08:43:09 +0300

    Добавьте файлы проекта.

---
+++ 5.6 Fin_Pr Module 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5._6_Fin_Pr_Module_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Data = YourData();
+        }
+
+        static (string, string, byte, bool, ) YourData()
+        {
+
+        }
+    }
+}
